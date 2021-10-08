7c8b7e644bf3898fe69c9b2cba6097597d2239b2
justengel <jtengel08@gmail.com>
2021-10-08 13:15:07 -0400

    Updated to version 2.1.1

--- resource_man/__meta__.py
+++ resource_man/__meta__.py
@@ -1,5 +1,5 @@
 name = 'resource_man'
-version = '2.1.0'
+version = '2.1.1'
 description = 'Python resource manager to work with the standard library for regular python code and executables.'
 url = 'https://github.com/justengel/resource_man'
 author = 'Justin Engel'
